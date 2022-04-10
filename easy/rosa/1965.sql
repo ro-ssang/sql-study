03558e2dfbb2a3c5bc13828f8eac09fab5799963
Rosang <fashionfish@naver.com>
2022-04-10 11:28:20 +0900

    1965번 문제풀이

---
+++ easy/rosa/1965.sql
@@ -0,0 +1,17 @@
+(
+    SELECT
+        e.employee_id
+    FROM Employees e
+    LEFT JOIN Salaries s ON e.employee_id = s.employee_id
+    WHERE s.salary IS NULL
+)
+UNION
+(
+    SELECT
+        s.employee_id
+    FROM Employees e
+    RIGHT JOIN Salaries s ON e.employee_id = s.employee_id
+    WHERE e.name IS NULL
+)
+ORDER BY employee_id ASC
+;
