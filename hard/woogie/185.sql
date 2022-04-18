4d746d020ea053ec268a346fa5f17b6e3be9f8af
woogieOnAndOn <123wodnr@naver.com>
2022-04-18 21:06:27 +0900

    185, 262, 1393

---
+++ hard/woogie/185.sql
@@ -0,0 +1,29 @@
+Create table If Not Exists Employee (id int, name varchar(255), salary int, departmentId int);
+Create table If Not Exists Department (id int, name varchar(255));
+Truncate table Employee;
+insert into Employee (id, name, salary, departmentId) values (1, 'Joe', 85000, 1);
+insert into Employee (id, name, salary, departmentId) values (2, 'Henry', 80000, 2);
+insert into Employee (id, name, salary, departmentId) values (3, 'Sam', 60000, 2);
+insert into Employee (id, name, salary, departmentId) values (4, 'Max', 90000, 1);
+insert into Employee (id, name, salary, departmentId) values (5, 'Janet', 69000, 1);
+insert into Employee (id, name, salary, departmentId) values (6, 'Randy', 85000, 1);
+insert into Employee (id, name, salary, departmentId) values (7, 'Will', 70000, 1);
+Truncate table Department;
+insert into Department (id, name) values (1, 'IT');
+insert into Department (id, name) values (2, 'Sales');
+
+SELECT 
+	t1.Department
+	,t1.Employee
+	,t1.Salary
+FROM (
+	SELECT 
+		d.name AS 'Department'
+		,e.name AS 'Employee'
+		,e.salary AS 'Salary'
+		,DENSE_RANK() OVER(PARTITION BY e.departmentId ORDER BY e.salary DESC) AS 'Rank'
+	FROM Employee e 
+	JOIN Department d ON e.departmentId = d.id
+) AS t1
+WHERE t1.Rank <= 3
+;
